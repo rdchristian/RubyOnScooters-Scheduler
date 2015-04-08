07b9d2a73215694f9ed48883338ad3f225bb1ff2
andrewnemec <andrewnemec7@gmail.com>
2015-04-08 10:04:22 -0500

    Attempt at BDD with Cuke.

--- db/schema.rb
+++ db/schema.rb
@@ -52,21 +52,6 @@ ActiveRecord::Schema.define(version: 20150402225412) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "resources", force: :cascade do |t|
-    t.string   "name"
-    t.string   "description"
-    t.integer  "numberOf"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "rooms", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "room_number"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
