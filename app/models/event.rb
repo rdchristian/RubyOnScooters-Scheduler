623bc17390f1b357d6f21a4550e19b09270c1216
Shayan Sayahi <ss.synix@gmail.com>
2015-04-11 23:59:18 -0500

    The association between a User and Events is working

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,17 @@
 class Event < ActiveRecord::Base
-  attr_accessible :title, :description, :start, :end
-  belongs_to :creator, :class_name => :User,:foreign_key => "user_id"
+  attr_accessible :title, :description, :start, :end, :creator, :creator_name
+  
+  belongs_to :creator, :class_name => :User, :foreign_key => "user_id"  
   has_and_belongs_to_many :resources
   has_and_belongs_to_many :facilities
+
+  def creator_name
+    return self.creator.name if self.creator
+    'N/A'
+  end
+
+  def creator_name=(user_name)
+    user = User.where(name: user_name).first
+    if user then self.creator = user end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -52,6 +52,21 @@ ActiveRecord::Schema.define(version: 20150402225412) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "resources", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "numberOf"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "room_number"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
