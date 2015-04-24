0f3d5cec71e969aa81549c4843efd9abed435956
rdchristian <d_christian2012@yahoo.com>
2015-04-24 15:21:04 -0500

    Removing merge leftovers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-<<<<<<< HEAD
 #sample_facilities = [
 #  {:name => 'Chair', :description => 'Used for sitting'},
 #  {:name => 'Video Projector', :description => 'Thingamajigy to project light on clear flat surfaces'},
@@ -15,13 +14,9 @@
 
 sample_users = [
 	{:name => 'Admin', :email => 'admin@email.com', :password => 'AdminPass', :password_confirmation => 'AdminPass', :user_level => 2, :activated => true}
-=======
+
 sample_facilities = [
-  {:name => 'Chair', :description => 'Used for sitting'},
-  {:name => 'Video Projector', :description => 'Thingamajigy to project light on clear flat surfaces'},
-  {:name => 'Poor Volunteer', :description => 'Free labor'},
-  {:name => 'Auditorium Corner 4a', :description => 'Boring boring b0r1nG'}
->>>>>>> search
+  {:name => 'Auditorium', :description => 'Big Room'}
 ]
 
 sample_facilities.each do |facility|
