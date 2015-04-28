fd0b802062316dabfe284328fa71fdd58a890f78
Shayan Sayahi <ss.synix@gmail.com>
2015-04-27 19:09:02 -0500

    Made dan's event controller actions private

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -75,20 +75,6 @@ class EventsController < ApplicationController
     end
   end
 
-  def approve
-    set_event
-    @event.approved = true;
-    @event.save!
-    redirect_to admin_path
-  end
-
-  def check_in
-    set_event
-    @event.checked_in = true;
-    @event.save!
-    redirect_to ""
-  end
-
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -110,6 +96,20 @@ class EventsController < ApplicationController
       else
         is_admin? ? Event : current_user.events
       end
+    end
+
+    def approve
+      set_event
+      @event.approved = true;
+      @event.save!
+      redirect_to admin_path
+    end
+
+    def check_in
+      set_event
+      @event.checked_in = true;
+      @event.save!
+      redirect_to ""
     end
 
     helper_method :format_fields
