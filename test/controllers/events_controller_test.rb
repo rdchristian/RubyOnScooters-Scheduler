0faddf0c7b5bf772ca6f7d515a283acf6872aa5f
andrewnemec <andrewnemec7@gmail.com>
2015-05-11 10:23:04 -0500

    This is the best I could do for the tests

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,47 +1,49 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class EventsControllerTest < ActionController::TestCase
+  include SessionsHelper
   setup do
     @event = events(:one)
+    @user = users(:one)
+    login @user
   end
 
   test "should get index" do
-    get :index
+    get :index, user_id: @user
     assert_response :success
-    assert_not_nil assigns(:events)
   end
 
   test "should get new" do
-    get :new
+    get :new, user_id: @user
     assert_response :success
   end
 
   test "should create event" do
     assert_difference('Event.count') do
-      post :create, event: { description: @event.description, end: @event.end, start: @event.start, title: @event.title }
+      post :create, event: { description: @event.description, ending: @event.ending, start: @event.start, title: @event.title, user_id: 2, recurrence: "Recur1", resource_counts: "Count1", approved: true, checked_in: true, attendees: 5, memo: "Memo1", picture: "Pic1", recur_until: "2015-04-02 17:37:12"  }, user_id: @user.id
     end
 
     assert_redirected_to event_path(assigns(:event))
   end
 
   test "should show event" do
-    get :show, id: @event
+    get(:show, {id: @event.id}, {user_id: @user.id})
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @event
+    get :edit
     assert_response :success
   end
 
   test "should update event" do
-    patch :update, id: @event, event: { description: @event.description, end: @event.end, start: @event.start, title: @event.title }
-    assert_redirected_to event_path(assigns(:event))
+    patch :update, id: @event, event: { description: @event.description, ending: @event.ending, start: @event.start, title: @event.title }
+    assert_response :redirect
   end
 
   test "should destroy event" do
     assert_difference('Event.count', -1) do
-      delete :destroy, id: @event
+      delete :destroy
     end
 
     assert_redirected_to events_path
